83ef4e31e2d9cd7d990083b13235a74991f6cef6
fvaron69 <fredyalberbaron@hotmail.com>
2019-01-21 16:06:32 -0500

    Update index.php

--- index.php
+++ index.php
@@ -7,27 +7,6 @@
          <link rel=”stylesheet” type=”text/css” href=”http://fonts.googleapis.com/css?family=Rokkit”>
          <link href="style.css" rel="stylesheet">
         </head>
-        <script>
-         //funcion filter 
-           
-        var words = [3, 12, 45, 7];
-        
-        const result = words.filter(c => c >= 10);
-        
-
-        console.log(result);
-// expected output: Array ["exuberant", "destruction", "present"]
-    //Hipotenusa
-        var a = 3;
-        var b = 4; 
-        console.log(Math.hypot(a,b));
-        //filtrar arreglo
-
-        //contar caracteres
-        var char = "Hello";
-        var str  = "l";
-        console.log(char.split(str).length-1);
-        </script>
     <body>
      <br>   
     <img	src="IMG_20190120_090501.jpg">
