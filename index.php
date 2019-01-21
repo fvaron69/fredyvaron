ab6d9bea55332b14098699939dac2fa29f03bfeb
fvaron69 <fredyalberbaron@hotmail.com>
2019-01-21 15:58:47 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+
+<html>
+    <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Pagina</title>
+         <meta name="viewport" content="width=device-width, initial-scale=1">
+         <link rel=”stylesheet” type=”text/css” href=”http://fonts.googleapis.com/css?family=Rokkit”>
+         <link href="style.css" rel="stylesheet">
+        </head>
+        <script>
+         //funcion filter 
+           
+        var words = [3, 12, 45, 7];
+        
+        const result = words.filter(c => c >= 10);
+        
+
+        console.log(result);
+// expected output: Array ["exuberant", "destruction", "present"]
+    //Hipotenusa
+        var a = 3;
+        var b = 4; 
+        console.log(Math.hypot(a,b));
+        //filtrar arreglo
+
+        //contar caracteres
+        var char = "Hello";
+        var str  = "l";
+        console.log(char.split(str).length-1);
+        </script>
+    <body>
+     <br>   
+    <img	src="IMG_20190120_090501.jpg">
+ <h2>Fredy Alberto</h2>
+ <h4>Guamo - Tolima</h4>
+    </body>
+</html>
